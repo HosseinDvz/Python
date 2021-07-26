55b3db3c5a2239284e295d94d32d6e2d36fb23ee
HosseinDvz <62449282+HosseinDvz@users.noreply.github.com>
2021-07-26 17:24:06 -0400

    Update DNN-Classification.py

--- Data Science/Classification/DNN-Classification.py
+++ Data Science/Classification/DNN-Classification.py
@@ -28,8 +28,7 @@ debug = 0
 
 
 
-#Choosing two books from Gutenberg Corpus
-#book_id1,book_id2,book_id3 = input("Choose a number between 0-17 separated by space: ").split()
+#Choosing six books from Gutenberg Corpus
 text1 = nltk.corpus.gutenberg.raw(nltk.corpus.gutenberg.fileids()[int(4)])
 text2 = nltk.corpus.gutenberg.raw(nltk.corpus.gutenberg.fileids()[int(7)])
 text3 = nltk.corpus.gutenberg.raw(nltk.corpus.gutenberg.fileids()[int(10)])
@@ -81,6 +80,7 @@ print(df_train)
 #shuffling the dataframe
 df_train = df_train.sample(frac=1)
 
+#Data Cleaninig
 def clean_text(x):
     #removing non-alphabet characters except "'"
     text = re.sub(r"[^a-zA-z'\s]", '', x)
@@ -89,7 +89,7 @@ def clean_text(x):
     # replacing multiple spaces with one space
     text = re.sub(r'\s+', ' ', text)
     return text
-x = clean_text("ai3434,n%'t a   ")
+#x = clean_text("ai3434,n%'t a   ")
 
 #Replacing contractions with extended form
 contraction_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have"}
@@ -115,6 +115,7 @@ df_train["Text"] = df_train["Text"].apply(lambda x: clean_text(x))
 df_train["Text"] = df_train["Text"].apply(lambda x: replace_contractions(x))
 
 
+#preparing the data for modelling
 from sklearn.model_selection import train_test_split
 
 train_X, test_X, train_y, test_y = train_test_split(df_train['Text'], df_train['Author'],
@@ -169,10 +170,7 @@ def load_glove(word_index):
     return embedding_matrix
 
 
-if debug:
-    embedding_matrix = np.random.randn(120000,100)
-else:
-    embedding_matrix = load_glove(tokenizer.word_index) 
+embedding_matrix = load_glove(tokenizer.word_index) 
 #embedding_matrix.shape
 
 #***********************************************************    
